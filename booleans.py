11f6881e485a211df0f672aa869b7d31c998af5d
MOBrien1 <67173712+MOBrien1@users.noreply.github.com>
2020-07-01 14:24:22 +0800

    Boolean activity Q2

--- booleans.py
+++ booleans.py
@@ -1,1 +1,34 @@
 #booleans
+moths_in_house = True
+mitch_is_home = False
+
+if moths_in_house is True:
+    print("Get the moths!")
+
+if mitch_is_home is False:
+    print("No threats detected.")
+
+moths_in_house = True
+mitch_is_home = True
+
+if moths_in_house and mitch_is_home:
+    print("HOOMAN! Help me get the moths!")
+
+moths_in_house = False
+mitch_is_home = False
+
+if not moths_in_house and not mitch_is_home:
+    print("No threats detected.")
+
+moths_in_house = True
+mitch_is_home = False
+
+if moths_in_house and not mitch_is_home:
+    print("Meooooooowwwwwww! Hissssssssss!")
+
+moths_in_house = False
+mitch_is_home = True
+
+if not moths_in_house and mitch_is_home:
+    print("Climb on Mitch.")
+    
